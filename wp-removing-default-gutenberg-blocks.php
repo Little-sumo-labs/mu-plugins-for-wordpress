278c112685c54cedf8d69de56ee6808ced83517c
Guillaume RICHARD <g.jf.richard@gmail.com>
2020-08-12 11:18:56 +0200

    Limitation des blocks à certains rôles

--- wp-removing-default-gutenberg-blocks.php
+++ wp-removing-default-gutenberg-blocks.php
@@ -8,7 +8,7 @@
  * Plugin URI:  https://guillaume-richard.fr/
  * Plugin Name: WP Removing Default Gutenberg Blocks
  * Description: Removing Default Gutenberg Blocks
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Misha Rudrastyh
  * Author URI:  https://rudrastyh.com/gutenberg/remove-default-blocks.html
  * License:     GNU General Public License v3 or later
@@ -20,23 +20,26 @@
 // Basic security, prevents file from being loaded directly.
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
-
-// add_filter( 'allowed_block_types', 'misha_allowed_block_types', 10, 2 );
-
 function misha_allowed_block_types( $allowed_blocks, $post ) {
+    if ( !is_user_logged_in() )
+        wp_redirect('wp-login.php');
 
-    $allowed_blocks = array(
-        'core/image',
-        'core/paragraph',
-        'core/heading',
-        'core/list'
-    );
+    $current_user = wp_get_current_user();
+    if ($current_user->roles[0] !== "administrator") {
+        $allowed_blocks = array(
+            'core/image',
+            'core/paragraph',
+            'core/heading',
+            'core/list'
+        );
 
-    if( $post->post_type === 'page' ) {
-        $allowed_blocks[] = 'core/shortcode';
+        if( $post->post_type === 'page' ) {
+            $allowed_blocks[] = 'core/shortcode';
+        }
+
+        return $allowed_blocks;
     }
-
-    return $allowed_blocks;
-
+    return;
 }
 
+add_filter( 'allowed_block_types', 'misha_allowed_block_types', 10, 2 );
